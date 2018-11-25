d28b4d8979194e14c33cab81ef8c48e035d9e74b
Piotr Dopierala <ptr.dopierala@gmail.com>
2018-11-25 13:56:58 +0100

    - added second method of parsing (based on String.split) - fixed edge cases, added new tests

--- src/main/java/pl/dopierala/EquationsParser.java
+++ src/main/java/pl/dopierala/EquationsParser.java
@@ -9,40 +9,75 @@ public class EquationsParser {
     private double b;
 
 
-
     public static Equation parse(String toParse) {
+        double a = 0;
+        double b = 0;
+        double c = 0;
 
-        double a=0,b=0,c=0;
+        //5x+6=100;
+        String[] eqSplit = toParse.split("=");
+        c = Double.parseDouble(eqSplit[1]);
 
+        boolean hasX = eqSplit[0].contains("x");
+        boolean hasmX = eqSplit[0].contains("-x");
+        String[] xSplit = eqSplit[0].split("x");
+        if (xSplit.length == 2) {
+            a = Double.parseDouble(xSplit[0]);
+            b = Double.parseDouble(xSplit[1]);
+        } else { //x not found split is one element, check if its a or b
+            if (xSplit.length == 1 && !xSplit[0].equals("-")) {
+                if (hasX)
+                    a = Double.parseDouble(xSplit[0]);
+                else
+                    b = Double.parseDouble(xSplit[0]);
+            }else{ //x or -x without number
+                if(hasmX)
+                    a=-1;
+                else
+                    a=1;
+            }
+        }
 
-        Pattern ptForA = Pattern.compile("-?\\d+x(\\+|\\=)");
-        Pattern ptForB = Pattern.compile("(\\+|-)\\d+=");
+        return new Equation(a, b, c);
+    }
+
+    public static Equation parseRegExp(String toParse) {
+
+        double a = 0, b = 0, c = 0;
+
+        Pattern ptForA = Pattern.compile("-?\\d+x");
+        Pattern ptForB = Pattern.compile("(\\+|-|^)\\d+=");
         Pattern ptForC = Pattern.compile("=-?\\d+$");
         Matcher mForA = ptForA.matcher(toParse);
         Matcher mForB = ptForB.matcher(toParse);
         Matcher mForC = ptForC.matcher(toParse);
 
-        if(mForA.find()) {
+        if (mForA.find()) {
             String strA = mForA.group();
-            a = Double.valueOf(strA.substring(0,strA.length()-2));
-        }else
-            a=1;
+            a = Double.valueOf(strA.substring(0, strA.length() - 1));
+        } else {
+            if (toParse.contains("x"))
+                if (toParse.contains("-x"))
+                    a = -1;
+                else
+                    a = 1;
+            else
+                a = 0;
+        }
 
 
-        if(mForB.find()) {
+        if (mForB.find()) {
             String strB = mForB.group();
-            b = Double.valueOf(strB.substring(0,strB.length()-1));
-        }
-        else
-            b=0;
+            b = Double.valueOf(strB.substring(0, strB.length() - 1));
+        } else
+            b = 0;
 
-        if(mForC.find()) {
+        if (mForC.find()) {
             String strC = mForC.group();
-            c = Double.valueOf(strC.substring(1,strC.length()));
-        }
-        else
+            c = Double.valueOf(strC.substring(1, strC.length()));
+        } else
             throw new IncorrectEquationException("Missing parameter after '=' sign. ");
 
-        return new Equation(a,b,c);
+        return new Equation(a, b, c);
     }
 }
--- src/test/java/pl/dopierala/EquationsParserTest.java
+++ src/test/java/pl/dopierala/EquationsParserTest.java
@@ -13,15 +13,69 @@ public class EquationsParserTest {
     }
 
     @Test
-    public void sholudParseEquation5x6Eq100(){
+    public void shouldParseEquation5x6Eq100() {
         //given
-        final String toParse ="5x+6=100";
-        final Equation expectedEquation = new Equation(5,6,100);
+        final String equation = "5x+6=100";
+        final Equation expectedEquation = new Equation(5, 6, 100);
 
         //when
-        Equation result = EquationsParser.parse(toParse);
-        //then
-        Assert.assertEquals(expectedEquation,result);
+        Equation result = eqParser.parse(equation);
 
+        //then
+        Assert.assertEquals(expectedEquation, result);
     }
+
+    @Test
+    public void shouldParseEquation5xEq100() {
+        //given
+        final String equation = "5x=100";
+        final Equation expectedEquation = new Equation(5, 0, 100);
+
+        //when
+        Equation result = eqParser.parse(equation);
+
+        //then
+        Assert.assertEquals(expectedEquation, result);
+    }
+
+    @Test
+    public void shouldParseEquation6Eq100() {
+        //given
+        final String equation = "5=100";
+        final Equation expectedEquation = new Equation(0, 5, 100);
+
+        //when
+        Equation result = eqParser.parse(equation);
+
+        //then
+        Assert.assertEquals(expectedEquation, result);
+    }
+
+    @Test
+    public void shouldParseEquationXEq100() {
+        //given
+        final String equation = "x=200";
+        final Equation expectedEquation = new Equation(1, 0, 200);
+
+        //when
+        Equation result = eqParser.parse(equation);
+
+        //then
+        Assert.assertEquals(expectedEquation, result);
+    }
+
+    @Test
+    public void shouldParseEquationMinusXEq100() {
+        //given
+        final String equation = "-x=100";
+        final Equation expectedEquation = new Equation(-1, 0, 100);
+
+        //when
+        Equation result = eqParser.parse(equation);
+
+        //then
+        Assert.assertEquals(expectedEquation, result);
+    }
+
 }
+
